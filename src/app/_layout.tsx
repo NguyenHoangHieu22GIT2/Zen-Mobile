6d4570bfc334861fbb706d23d2d401909eee73e3
hoanghieu <hoanghieufro@gmail.com>
2024-02-12 17:09:36 +0700

    Initialize

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,7 @@
+import { View } from "react-native";
 import "../global.css";
 import { Slot } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Layout() {
   return <Slot />;
