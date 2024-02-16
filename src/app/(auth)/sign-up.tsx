1f84e1b2ea0be9e68bb0ecebaf4e095b31839aa5
hoanghieu <hoanghieufro@gmail.com>
2024-02-16 17:07:14 +0700

    Add Eslint

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -2,9 +2,9 @@ import { View, Text } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
-
 export default function SignUpForm() {
   const router = useRouter();
+
   return (
     <View className="flex-1 bg-white justify-center items-center">
       <Text>This is SignUpForm</Text>
