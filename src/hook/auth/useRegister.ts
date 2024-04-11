f4855c9a88ad67d08b778ad895e272264c7c106b
MinhNhat-22IT205 <126837678+MinhNhat-22IT205@users.noreply.github.com>
2024-04-11 22:31:08 +0700

    about to finish home

--- src/hook/auth/useRegister.ts
+++ src/hook/auth/useRegister.ts
@@ -4,6 +4,7 @@ import { zRegisterInputs, ztRegisterInputs } from "@/libs/zod";
 import { useState } from "react";
 import { router } from "expo-router";
 import toast from "@/utils/toast/toast";
+import { CanceledError } from "axios";
 export function useRegister() {
   const [inputs, setInputs] = useState<ztRegisterInputs>({
     username: "brangto",
@@ -19,28 +20,32 @@ export function useRegister() {
 
   // TODO: FINISH WITH NOTIFICATION
   async function submitRegister() {
-    toast.success({ message: "Successfully signed up!" });
-    router.push("/popular");
-    // const zodResult = zRegisterInputs.safeParse(inputs);
-    // if (!zodResult.success) return false;
-    // try {
-    //   const result = await http.post(
-    //     process.env.HTTP_ENDPOINT_REGISTER,
-    //     inputs
-    //   );
-    //   if (result.status !== HTTP_POST_SUCCESS) {
-    //     console.log("Failed Register");
-    //   } else {
-    //     toast.success({ message: "Successfully signed up!" });
-    //     router.push("/login");
-    //   }
-    // } catch (error) {
-    //   toast.danger({
-    //     message: error.message,
-    //     subMessage: "Breh",
-    //     duration: 3000
-    //   });
-    // }
+    // toast.success({ message: "Successfully signed up!" });
+    // router.push("/popular");
+    const zodResult = zRegisterInputs.safeParse(inputs);
+    if (!zodResult.success) return false;
+    try {
+      const result = await http.post(
+        process.env.HTTP_ENDPOINT_REGISTER,
+        inputs
+      );
+      console.log(result);
+      // if (result.status !== HTTP_POST_SUCCESS) {
+      //   console.log("Failed Register");
+      // } else {
+      toast.success({ message: "Successfully signed up!" });
+      router.push("/login");
+      // }
+    } catch (error) {
+      toast.danger({
+        message: error.message,
+        subMessage: "Breh",
+        duration: 3000
+      });
+      console.log(error.message);
+      console.log(error.request);
+      console.log(error.error);
+    }
   }
 
   return {
