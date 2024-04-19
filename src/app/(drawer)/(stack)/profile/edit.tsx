d0cf834ca485441c394f94244700760b685daf9b
MinhNhat-22IT205 <126837678+MinhNhat-22IT205@users.noreply.github.com>
2024-04-19 14:45:05 +0700

    added EditProfile stack

--- src/app/(drawer)/(stack)/profile/edit.tsx
+++ src/app/(drawer)/(stack)/profile/edit.tsx
@@ -1,18 +1,29 @@
 import {
   AuthTextInput,
-  BackSvg,
   FontText,
-  ProfileAvatarImage,
   RectangleButton,
   TopWrapperView
 } from "@/components";
+import ChangableProfileAvatarImage from "@/components/profile/images/ChangableProfileAvatarImage";
 import { COLORS, IMAGES } from "@/constants";
+import { useState } from "react";
 
 export default function edit() {
+  const [selectedImage, setSelectedImage] = useState(IMAGES.fakeavatar);
+
   return (
-    <TopWrapperView style={{ width: "100%" }} className="justify-center px-3">
-      <ProfileAvatarImage source={IMAGES.fakeavatar} />
-      <FontText className="font-bold text-xl">John Doe</FontText>
+    <TopWrapperView style={{ height: "100%" }} className="items-center px-4">
+      <ChangableProfileAvatarImage
+        setSelectedImage={setSelectedImage}
+        source={
+          selectedImage == IMAGES.fakeavatar
+            ? IMAGES.fakeavatar
+            : { uri: selectedImage }
+        }
+      />
+      <FontText className="font-bold text-xl text-center my-2">
+        John Doe
+      </FontText>
       <AuthTextInput
         SVGIconElement={<></>}
         label="Username"
@@ -29,9 +40,15 @@ export default function edit() {
         SVGIconElement={<></>}
         label="About me..."
         selectionColor={COLORS.primary}
+        multiline
         className="mb-3"
       />
-      <RectangleButton text="Save" onPress={() => {}} />
+      <RectangleButton
+        style={{ width: "100%" }}
+        textStyle="font-bold text-lg"
+        text="Save"
+        onPress={() => {}}
+      />
     </TopWrapperView>
   );
 }
